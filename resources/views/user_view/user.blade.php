3312caa67a33e22151de7f0a463118eee3467460
Andres <andres.sikka98@gmail.com>
2020-09-29 16:30:00 +0300

    first commit

--- resources/views/user_view/user.blade.php
+++ resources/views/user_view/user.blade.php
@@ -7,7 +7,7 @@
 				<div class="card-body">
 					<h4 class="card-title">{{$article->Title}}</h4>
 					<p class="card-text">{{$article->Body}}</p>
-					<a href="#!" class="btn btn-primary">Open article</a>
+					<a href="#!" class="btn btn-primary">Open articl</a>
 				</div>
 			</div>
 		@endforeach
